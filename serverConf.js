520df93874235258c3680eb9edd6eaa88ffb5a2e
perfectdev0712 <greenland712@outlook.com>
2021-06-15 20:22:52 +0300

    asdf

--- serverConf.js
+++ serverConf.js
@@ -1,8 +1,8 @@
 module.exports = {
     DIR: __dirname,
     ServerPort: 5000,
-    // DBCONNECT: "mongodb://localhost:27017/carepeople",
-    DBCONNECT: "mongodb://carepeople:j6P3yKPdkCItOjdz@139.177.196.64:27731/carepeople?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256",
+    DBCONNECT: "mongodb://localhost:27017/carepeople",
+    // DBCONNECT: "mongodb://carepeople:j6P3yKPdkCItOjdz@139.177.196.64:27731/carepeople?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256",
 
     "BASEURL" : __dirname+"/uploads/",
     "ADMINPASSMETHOD" : "admin",
