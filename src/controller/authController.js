ee0c58c3c694280d86eb1ab2c76dd6e43932ab25
perfectdev0712 <greenland712@outlook.com>
2021-06-16 06:02:28 +0300

    asfd

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -4,14 +4,29 @@ import { mainUser } from "../models/index";
 export const signUp = async (req, res) => {
     let fData = req.body;
     let cFlag = await dbController.bFindOne(mainUser, { email: fData.email })
-    if(cFlag) {
+    if (cFlag) {
         return res.json({ status: false, data: "this email is exist" })
     } else {
         let sFlag = await dbController.bSaveData(fData, mainUser)
-        if(sFlag) {
+        if (sFlag) {
             return res.json({ status: true, data: "Successfuly signup", userInfo: sFlag })
         } else {
             return res.json({ status: false, data: "this is server error" })
         }
+    }
+}
+
+export const signIn = async (req, res) => {
+    let fData = req.body;
+    let cFlag = await dbController.bFindOne(mainUser, { email: fData.email })
+    if (cFlag) {
+        let pFlag = await dbController.bFindOne(mainUser, { email: fData.email, password: fData.password })
+        if (pFlag) {
+            return res.json({ status: true, data: "Success", userInfo: pFlag })
+        } else {
+            return res.json({ status: false, data: "Password is incorrect" })
+        }
+    } else {
+        return res.json({ status: false, data: "This email is no exist" })
     }
 }
--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -4,5 +4,6 @@ import * as authController from "../controller/authController";
 const Router = routerx();
 
 Router.post("/signup", authController.signUp);
+Router.post("/signin", authController.signIn);
  
 export default Router;
