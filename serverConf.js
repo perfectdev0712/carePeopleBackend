1660161deaedbc5b1535f9818ccc143a8a2eac2b
perfectdev0712 <greenland712@outlook.com>
2021-07-02 17:48:01 +0300

    Update serverConf.js

--- serverConf.js
+++ serverConf.js
@@ -3,7 +3,7 @@ module.exports = {
     ServerPort: 5000,
     DBCONNECT: 
     // process.env.NODE_ENV === "development" ? 
-    //     "mongodb://localhost:27017/carepeople"
+        "mongodb://localhost:27017/carepeople"
     //      :
-        "mongodb://carepeople:j6P3yKPdkCItOjdz@139.177.196.64:27731/carepeople?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256",
+        // "mongodb://carepeople:j6P3yKPdkCItOjdz@139.177.196.64:27731/carepeople?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256",
 }
